18a3c6978247f47e9715c7f2bb3eec0722f930dd
sammyrhymes <samuelmwendwa5996@gmail.com>
2023-10-03 19:28:46 +0300

    Added templates to the polls app     - it now displays the question and answers     - voting to implimented soon

--- new/polls/views.py
+++ new/polls/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpRequest, HttpResponse
+from .models import Question, Choice
 # Create your views here.
+def index(request):
+    questions = Question.objects.order_by("-pub_date")[0:]
+    title = "Home"
+    context = {"questions":questions, "title" : title}
+    return render(request, "polls/index.html", context)
+
+def details(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    choices = question.choice_set.all()
+    context = {"question": question, "title": "details", "choices": choices}
+    return render(request, "polls/details.html", context)
+
+def results(request, question_id):
+    response = f"You are looking for results for {question_id}"
+    return HttpResponse(response)
+
+def vote(request, question_id):
+    return HttpResponse(f"You are voting on question {question_id}")
